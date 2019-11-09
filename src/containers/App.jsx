cbaab0287a4aa833eeae646acc8f0adb2b3a8a30
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2019-11-09 16:41:15 -0400

    inicializando Proyecto, ADD Header y Buscador

---
+++ src/containers/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../component/Header';
+import Search from '../component/Search.jsx';
+import '../assets/styles/App.scss';
+
+
+
+const App = () => (
+    <div className="App">
+        <Header />
+        < Search />
+
+    </div>
+);
+
+
+
+export default App; 
