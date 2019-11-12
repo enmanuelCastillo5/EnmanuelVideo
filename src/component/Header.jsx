fed4fcb2db99d458a330d04084fc8d4911d334f1
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2019-11-12 09:33:47 -0400

    cambiando elemnto a por LINK

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import '../assets/styles/component/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
@@ -6,7 +7,10 @@ import userIcon from '../assets/static/user-icon.png';
 
 const Header = () => (
     <header className="header">
+      <Link to='/'>
     <img className="header__img" src={logo} alt="Platzi Video" />
+    </Link>
+
     <div className="header__menu">
       <div className="header__menu--profile">
         <img src={userIcon} alt="" />
@@ -14,7 +18,7 @@ const Header = () => (
       </div>
       <ul>
         <li><a href="/">Cuenta</a></li>
-        <li><a href="/">Cerrar Sesión</a></li>
+        <li><Link to="/login">Iniciar Sesión</Link></li>
       </ul>
     </div>
   </header>
